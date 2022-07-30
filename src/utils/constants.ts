9103d954bd8b774923aaa118b1bce51e79a77517
Mohamed Sami <srmf2025@gmail.com>
2022-07-31 01:50:46 +0530

    add channelId to .env, include icon and game instruction to constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,7 +7,28 @@ if (!process.env.GUILD_ID) console.error("Bot requires GUILD_ID in .env");
 const token: string = process.env.TOKEN || "";
 const clientId: string = process.env.CLIENT_ID || "";
 const guildId: string = process.env.GUILD_ID || "";
+const channelId: string = process.env.GAME_CHANNEL_ID || "";
 const dbUri: string = process.env.DB_URI || "";
 const dbName: string = process.env.DB_NAME || "";
 
-export { token, clientId, guildId, dbUri, dbName };
+const iconURL =
+  "https://cdn.discordapp.com/attachments/871801727974785055/937070492366561352/outline_place_white_24dp.png";
+const gameInstructions = [
+  "You have a week to find the city in the image attached below.",
+  "You are allowed to guess once per day (resets at 00:00 (12:00 AM) your local time)",
+  "Provide a reason why you think it is the city you guess.",
+  "Create a ticket and send the answer with a reason (not just that you know, for it to be counted).",
+  "A Staff will check the correctness of your answer and award you points.",
+  "After you've got the correct answer, do NOT close the thread. DON'T create a ticket if you are not going to make a guess",
+];
+
+export {
+  token,
+  clientId,
+  guildId,
+  channelId,
+  dbUri,
+  dbName,
+  iconURL,
+  gameInstructions,
+};
