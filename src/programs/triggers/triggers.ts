d110c89467ab3de530d2dceac3019f1e4b02f282
Mohamed Sami <srmf2025@gmail.com>
2022-08-24 14:01:09 +0530

    fix: change in variable name

--- src/programs/triggers/triggers.ts
+++ src/programs/triggers/triggers.ts
@@ -2,16 +2,16 @@ import { Message } from "discord.js";
 import { getChatTriggers } from "./controllers";
 import { Trigger, TriggersWithResponse } from "./schema";
 
-let index: Array<TriggersWithResponse>;
+let triggers: Array<TriggersWithResponse>;
 let lastUpdated: number;
 
 export const loadChatTriggers = async () => {
   if (!lastUpdated || Date.now() - lastUpdated > 1000 * 60 * 30) {
-    index = (await getChatTriggers()) || [];
+    triggers = (await getChatTriggers()) || [];
     console.log("✔️Loaded Chat Triggers");
     lastUpdated = Date.now();
   }
-  return index;
+  return triggers;
 };
 
 export const getReplyToChatTrigger = async (message: Message) => {
